61a7b3ffa99b9fb0e2a411e3e98015e5a1e5eef8
Moh2700 <mohhabbes@gmail.com>
2026-01-05 21:15:07 +0000

    Creating an option for searching content

--- Js/main.js
+++ Js/main.js
@@ -66,6 +66,9 @@
 // ------------------- Events Grid for Lectures-------------------
 async function loadLectureEvents() {
 
+    const divsection = document.getElementById('tours');
+    divsection.style.display = 'none';
+
     const section = document.getElementById('events');
     section.style.display = 'block';
 
@@ -74,8 +77,6 @@ async function loadLectureEvents() {
         const response = await fetch('data/lectureevent.json');
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         const eventIndex = await response.json();
-
-       // alert(eventIndex.lecture);
 
         const isRTL = document.body.classList.contains('rtl') || document.documentElement.lang === 'ar';
         const noEventsMessage = isRTL ? 'لا توجد فعاليات قادمة في الوقت الحالي.' : 'No upcoming events at this time.';
@@ -90,8 +91,7 @@ async function loadLectureEvents() {
 
        
         for (const event of eventIndex.lectures) {
-            // alert ("" +`data/lecture_details/lecture${event.id}.json`);
-            
+           
             const detailResp = await fetch(`data/lecture_details/lecture${event.id}.json`);
             const details = detailResp.ok ? await detailResp.json() : { id: event.id, date: event.date, description: event.description };
             
@@ -120,75 +120,11 @@ async function loadLectureEvents() {
         grid.innerHTML = `<p style="color: #ff6b6b;">${msg}</p>`;
     }
 }
-//==================== End events Grid for Lectures================================
-
- // ------------------- Events Grid for Lectures-------------------
-
- /*
- async function loadTourEvents() {
-
-    const section = document.getElementById('tours');
-    section.style.display = 'block';
-
-    const grid = document.getElementById('tours-grid');
-    try {
-        const response = await fetch('data/toursevent.json');
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        const eventIndex = await response.json();
-
-        const isRTL = document.body.classList.contains('rtl') || document.documentElement.lang === 'ar';
-        const noEventsMessage = isRTL ? 'لا توجد فعاليات قادمة في الوقت الحالي.' : 'No upcoming events at this time.';
-
-
-        if (!eventIndex.tours || eventIndex.tours.length === 0) {
-            grid.innerHTML = `<p>${noEventsMessage}</p>`;
-            return;
-        }
-
-        grid.innerHTML = '';
-
-        for (const event of eventIndex.tours) {
-
-            const detailResp = await fetch(`data/tours_details/tours${event.id}.json`);
-            const details = detailResp.ok ? await detailResp.json() : { id: event.id, date: event.date, description: event.description };
-          
-            const card = document.createElement('div');
-            card.className = 'event-card';
-            card.innerHTML = `
-             
-            <img src="${details.src}" alt="${details.title}">
-            <div id="${details.id}" class="event-card-content">
-                <h4>Title:${details.title}</h4>
-                <h2>Description: ${event.description}</h2>
-                <p><strong>Date:</strong> ${event.date}</p>
-                <p><strong>Price:</strong> ${details.price}</p>
-                <p><strong>Duration:</strong>${details.duration}</p>
-                    
-            </div>
-
-            <button class="book-btn" onclick="openBooking('${details.id}')">
-                   Book / Register
-            </button>
-
-            `;
-            grid.appendChild(card);
-
-        }
-
-    RegisterEvent (eventid);
-
-    } catch (error) {
-        console.error("Could not load events:", error);
-        const isRTL = document.body.classList.contains('rtl') || document.documentElement.lang === 'ar';
-        const msg = isRTL ? 'عذرًا، لا نستطيع تحميل الفعاليات الآن. الرجاء المحاولة لاحقًا.' : "Sorry, we couldn't load the events. Please try again later.";
-        grid.innerHTML = `<p style="color: #ff6b6b;">${msg}</p>`;
-    }
-}
- */   
-//==================== End events Grid for Lectures================================
-
 
 async function loadTourEvent() {
+
+     const lecturesection = document.getElementById('events');
+     lecturesection.style.display = 'none';
 
     const section = document.getElementById('tours');
     section.style.display = 'block';
@@ -221,24 +157,28 @@ async function loadTourEvent() {
             card.innerHTML = `
              
             <img src="${event.src}" alt="${event.title}">
+
+            
             <div id="${event.id}" class="event-card-content">
                 <h4>Title:${event.title}</h4>
                 <h2>Description: ${event.description}</h2>
-                <p><strong>Total Members:</strong> ${event.maxbooking}</p>
+                <p><strong>Seats:</strong> ${event.seats}</p>
                 <p><strong>Date:</strong> ${event.date}</p>
                 <p><strong>Price:</strong> ${event.price}</p>
                 <p><strong>Duration:</strong>${event.duration}</p>
                     
             </div>
 
-            <button class="book-btn" onclick="openBooking('${event.id}')">
+            <button id = "btn${event.id}" class="book-btn" onclick="openBooking('${event.id}')">
                    Book / Register
             </button>
-
+          
             `;
             grid.appendChild(card);
 
         }
+
+        
 
     RegisterEvent (eventid);
 
@@ -255,19 +195,19 @@ async function loadTourEvent() {
 function RegisterEvent (eventid)
 {
     const grid = document.getElementById('bookingModal');
+    //grid.style.display ='block';
+     
+    // var regevent =` <div class="modal-content" class="modal" >
+    var regevent =` 
+    
+    <form id="BookingForm">
+      <div class="modal-content"  >
 
-    //alert ( eventid + " VVVVVVVV " + searchJSONSafe(eventid).title);
-
-
-
-    var regevent =` <div  class="modal-content" class="modal" >
-    <h2>Event Registration</h2>
-         
-    <form id="RegistrationForm">
-
-     <label>
+       <h2>Event Registration</h2>
+        
+      <label>
         Event ID
-        <input type="text" id="eventId"   disabled/>
+        <input type="text" id="eventId" value='${eventid}'  disabled/>
       </label>
 
       <label>
@@ -278,21 +218,24 @@ function RegisterEvent (eventid)
 
       <label>
         Full Name
-        <input type="text" id="fullname" required />
+        <input type="text" id="username" required />
       </label>
 
       <label>
         Email
-        <input type="email" id="email" required />
+        <input type="email" id="useremail" required />
       </label>
 
-       <div id="divbutton">
-         <p class="book-btn" onclick="confirmBooking()">Confirm</p>
-         <p class="book-btn" onclick="closeBooking()">Cancel</p>
-        </div>
-    </form>
-  </div>` ;
+      <div id="divbutton" " >
+         <p class="book-btn" id="cmdBooking"  onclick="confirmBooking('${eventid}')"  >Confirm</p>
+         <p class="book-btn" id ="cmdCancel" onclick="closeBooking()">Cancel</p>
+      </div>
+    
+      </div>
+      
+      </form>` ;
 
+  
    grid.innerHTML = regevent;
   
 }
@@ -307,24 +250,204 @@ function closeBooking() {
 function populateBookingform (evenid, tour) {
 
     document.getElementById("eventTitle").value = tour.title;
-     document.getElementById("eventId").value = tour.id;
+    document.getElementById("eventId").value = tour.id;
 
+    document.getElementById("eventId").setAttribute('value', tour.id);
+    document.getElementById("eventTitle").setAttribute('value', tour.title);
+
+}
+
+
+function updateBooking ()
+{
+ const fs = require("fs");
+
+// STEP 1: Reading JSON file 
+const users = require("/data/eventregistration/registration1.json");
+
+alert(users.length);
+
+// Defining new user 
+let user =
+{
+    name: "New User",
+    age: 30,
+    language: ["PHP", "Go", "JavaScript"]
+};
+
+// STEP 2: Adding new data to users object 
+users.push(user);
+
+alert(user);
+
+// STEP 3: Writing to a file 
+fs.writeFile(
+    "registration1.json",
+    JSON.stringify(users),
+    err => {
+        // Checking for errors 
+        if (err) throw err;
+
+        // Success 
+        console.log("Done writing");
+    }); 
 }
 
 function openBooking(eventid) {
       
     searchJSONSafe(eventid); 
+    mdlBooking.style.display = 'block';
    
-    mdlBooking.style.display = 'flex';
 }
 
-function confirmBooking () {
-       alert (eventid) ;
+function RenderTourBookings(eventid)  {
 
-       mdlBooking.style.display = "none";
-       form.reset();
+  const tourbooking = document.getElementById(eventid);
+  const myNodelist = tourbooking.querySelectorAll("p");
+
+  const tourgrid = document.getElementById('tours-grid');
+  
+  var str= "";
+
+  for (let i = 0; i < myNodelist.length; i++) {
+
+   if (i== 0) {
+    let num = myNodelist[i].innerText.replace(/[^0-9]/g, '');
+    num = parseInt(num);
+    //myNodelist[i].innerText = myNodelist[i].innerText;
+    //var alpha = myNodelist[i].innerText;
+    //answer = alpha.replace(/[^a-zA-Z]/g, '');
+    num--;
+
+    if (num == 0)  {
+      
+      document.getElementById("btn"+eventid).innerText = "Sold out";
+      document.getElementById("btn"+eventid ).disabled= true;
+      document.getElementById("btn"+eventid).style.cursor = 'not-allowed'; 
+
+      str = myNodelist[i].innerText = "Seats : " + num ;
+
+    } else {
+
+      str = myNodelist[i].innerText = "Seats : " + num ;
+
+    }
+   
+   }
+}
+return str;
 }
 
+function saveUserBooking()
+{
+  
+   const booked = [];
+    booked.push({id:document.getElementById("eventId").value.trim(), 
+    title:document.getElementById("eventTitle").value.trim(), 
+    username:document.getElementById("username").value.trim(), 
+    useremail:document.getElementById("useremail").value.trim(), 
+    date: new Date() });
+
+  return booked;  
+}
+
+function renderBookingList   (bookingList, booked)
+{
+  booked.forEach((b) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<strong>${b.username}</strong> booked <strong>${b.title}</strong>  on <strong>  (${b.date}) </strong>`;
+  bookingList.appendChild(li);
+  });
+  
+ 
+  return bookingList;
+}
+
+function confirmBooking (eventid) {
+      
+    const divsection = document.getElementById('tours-reservation');
+    divsection.style.display = 'block';
+
+    const toursgrid = document.getElementById('tours-reservation-grid');
+    const bookingList = document.getElementById("tourbookinglist");
+
+    
+    const frmBooking = document.getElementById('BookingForm');
+    const name = document.getElementById("username").value.trim();
+    const email = document.getElementById("useremail").value.trim();
+
+    
+ 
+        
+        if (!name) {
+          alert("Please enter your name");
+          return;
+        }
+
+        if (!email) {
+          alert("Please enter your email");
+          return;
+        }
+         
+        var id = document.getElementById("eventId").value.trim();
+        
+        RenderTourBookings(id);
+
+        var booked = saveUserBooking ();
+
+        renderBookingList(bookingList, booked);
+
+
+    toursgrid.appendChild(bookingList);
+    
+    frmBooking.reset();
+
+    mdlBooking.style.display = "none";
+
+}
+
+async function searchJSONSafe(keyword) {
+  try {
+    const res = await fetch("/data/tours_details/tours.json");
+    if (!res.ok) throw new Error("Failed to load JSON");
+
+    const data = await res.json();
+
+    const result = JSON.stringify(data);
+    
+    const tour = data.tours.find(u => u.id === keyword);
+
+    populateBookingform (eventid, tour);
+
+    return tour;
+    
+  } catch (err) {
+    alert( "------" + err);
+    return [];
+  }
+}
+
+
+// "/data/tours_details/tours.json"
+async function searchJSON(strid, strfilepath) {
+  try {
+    const res = await fetch(strfilepath);
+    if (!res.ok) throw new Error("Failed to load JSON");
+
+    const data = await res.json();
+
+    const result = JSON.stringify(data);
+    
+    const tour = data.tours.find(u => u.id === strid);
+
+    return tour;
+    
+  } catch (err) {
+     return err;
+  }
+}
+
+/*
 function useful ()  {
 var sitePersonel = {};
 var employees = []
@@ -342,11 +465,11 @@ sitePersonel.employees.push(employee);
 
 var manager = "Jane Doe";
 sitePersonel.employees[0].manager = manager;
-
+*/
 //console.log(sitePersonel);
 
 //alert(JSON.stringify(sitePersonel));
-} 
+
  
 /*
 async function fetchMoviesBadStatus() {
@@ -379,55 +502,7 @@ async function finddata(id) {
 */
 
 
-async function searchJSONSafe(keyword) {
-  try {
-    const res = await fetch("/data/tours_details/tours.json");
-    if (!res.ok) throw new Error("Failed to load JSON");
 
-    const data = await res.json();
-
-    const result = JSON.stringify(data);
-    
-    const tour = data.tours.find(u => u.id === keyword);
-
-
- //   document.getElementById("eventTitle").value = tour.title;
-     populateBookingform (eventid, tour);
-
-
-    return tour;
-    
-  } catch (err) {
-    alert( "------" + err);
-    return [];
-  }
-}
-
-
-navtoggle ();
-
-const mdlBooking = document.getElementById("bookingModal");
-
-
-
-class EventSelected {
-  constructor(eventid , eventName, eventDate) {
-    this.eventid = eventid;
-    this.eventName = eventName;
-    this.eventDate = new Date(eventDate);
-  }
-}
-
-
-async function getData() {
-  try {
-    const res =  fetch("./data/tours_details/tours.json");
-    const data = await res.json();
-    alert (data.tours.length);
-  } catch (err) {
-     alert("+++++++" + err);
-  }
-}
 
 class EventRegistration {
    constructor(eventName, eventDate, capacity = Infinity) {
@@ -504,3 +579,8 @@ eventReg.register({ name: "Bob", email: "bob@mail.com" });
 console.log(eventReg.listAttendees());
 console.log(eventReg.eventSummary());
 */
+
+
+navtoggle ();
+
+const mdlBooking = document.getElementById("bookingModal");
